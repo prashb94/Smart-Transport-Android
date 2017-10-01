f1b2801cef9db601566dd8fb54131e6205ba31d1
Prashanth B <prash_94@hotmail.com>
2017-10-01 08:17:56 +0200

    First commit, hello world accelerometer

---
+++ app/src/main/java/ch/ethz/vs/se/team7/carbonfootprintmonitor/MainActivity.java
@@ -0,0 +1,66 @@
+package ch.ethz.vs.se.team7.carbonfootprintmonitor;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView tvSpeed = (TextView) this.findViewById(R.id.textViewSpeed);
+        TextView tvLocation = (TextView) this.findViewById(R.id.textViewLocation);
+
+        if(location == null){
+            tvLocation.setText("-.-, -.-");
+            tvSpeed.setText("-.- m/s");
+        }
+        else
+        {
+            float currentSpeed = location.getSpeed();
+            tvSpeed.setText(currentSpeed + " m/s");
+            tvLocation.setText(location.getLatitude() + ", " + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
