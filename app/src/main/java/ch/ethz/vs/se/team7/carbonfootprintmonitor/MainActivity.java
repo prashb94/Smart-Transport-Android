2e805275bbdf8e84858a6b5ea65d5ec6e73dbbd8
Prashanth B <prash_94@hotmail.com>
2017-10-01 10:06:54 +0200

    Fix location

--- app/src/main/java/ch/ethz/vs/se/team7/carbonfootprintmonitor/MainActivity.java
+++ app/src/main/java/ch/ethz/vs/se/team7/carbonfootprintmonitor/MainActivity.java
@@ -2,65 +2,80 @@ package ch.ethz.vs.se.team7.carbonfootprintmonitor;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import static ch.ethz.vs.se.team7.carbonfootprintmonitor.R.string.location;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+
+public class MainActivity extends AppCompatActivity {
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private TextView tvLocation;
+    private TextView tvSpeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                tvLocation = (TextView) findViewById(R.id.textViewLocation);
+                tvSpeed = (TextView) findViewById(R.id.textViewSpeed);
+                tvLocation.setText(location.getLatitude() + ", " + location.getLongitude());
+                tvSpeed.setText(location.getSpeed() + " m/s");
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 10);
+                return;
+            } else {
+                locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
+            }
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        TextView tvSpeed = (TextView) this.findViewById(R.id.textViewSpeed);
-        TextView tvLocation = (TextView) this.findViewById(R.id.textViewLocation);
 
-        if(location == null){
-            tvLocation.setText("-.-, -.-");
-            tvSpeed.setText("-.- m/s");
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
+                    return;
+                }
         }
-        else
-        {
-            float currentSpeed = location.getSpeed();
-            tvSpeed.setText(currentSpeed + " m/s");
-            tvLocation.setText(location.getLatitude() + ", " + location.getLongitude());
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
     }
 }
