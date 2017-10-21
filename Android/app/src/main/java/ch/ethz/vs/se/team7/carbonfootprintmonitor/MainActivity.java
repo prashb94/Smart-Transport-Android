d291a085897f2508805cbb9cf43db3460d709f8d
rakshi00 <rakshi00@gmail.com>
2017-10-21 21:18:50 +0200

    Add Toggle button
    Add fab button
    Add debug options
    Add title style
    Connect location to textView onChange
    Add toggle methods in code to change legend

--- Android/app/src/main/java/ch/ethz/vs/se/team7/carbonfootprintmonitor/MainActivity.java
+++ Android/app/src/main/java/ch/ethz/vs/se/team7/carbonfootprintmonitor/MainActivity.java
@@ -12,28 +12,45 @@ import android.os.Build;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.ActivityRecognition;
 
-import static ch.ethz.vs.se.team7.carbonfootprintmonitor.R.string.location;
-
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
+    public GoogleApiClient mApiClient;
+
+    // debugging text views
+    private TextView tvDetectedActivity;
     private TextView tvLocation;
     private TextView tvSpeed;
-    public GoogleApiClient mApiClient;
-    private TextView tvDetectedActivity;
 
     private String username;
+    private FloatingActionButton energyToggleButton;
+    private boolean energyDisplayFlag;
+
+    // table contents MainActivity
+    private TextView legendTimeCO2;
+    private TextView legendDistanceEnergy;
+    private TextView walkValue1;
+    private TextView walkValue2;
+    private TextView bikeValue1;
+    private TextView bikeValue2;
+    private TextView carValue1;
+    private TextView carValue2;
+    private TextView tramValue1;
+    private TextView tramValue2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +62,35 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         username = intent.getStringExtra(LoginActivity.EXTRA_USERNAME);
         TextView welcomeUser = findViewById(R.id.welcome_user);
         welcomeUser.setText(getResources().getString(R.string.live_green, username));
+
+        // Initialize debugging textViews
+        tvSpeed = findViewById(R.id.textView_speed);
+        tvLocation = findViewById(R.id.textView_location);
+
+        // Initialize (CO2/energy) / (h/km) toggle button
+        energyToggleButton = findViewById(R.id.button_toggle_energy);
+        energyToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleEnergyDisplay();
+            }
+        });
+
+        // Initialize table TextViews
+        legendTimeCO2 = findViewById(R.id.table_text_0_1);
+        legendDistanceEnergy = findViewById(R.id.table_text_0_2);
+        walkValue1 = findViewById(R.id.table_text_1_1);
+        walkValue2 = findViewById(R.id.table_text_1_2);
+        bikeValue1 = findViewById(R.id.table_text_2_1);
+        bikeValue2 = findViewById(R.id.table_text_2_2);
+        carValue1 = findViewById(R.id.table_text_3_1);
+        carValue2 = findViewById(R.id.table_text_3_2);
+        tramValue1 = findViewById(R.id.table_text_4_1);
+        tramValue2 = findViewById(R.id.table_text_4_2);
+
+        energyDisplayFlag = false;
+
+
 
         mApiClient = new GoogleApiClient.Builder(this)
                 .addApi(ActivityRecognition.API)
@@ -58,10 +104,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
             @Override
             public void onLocationChanged(Location location) {
-                tvLocation = (TextView) findViewById(R.id.textViewLocation);
-                tvSpeed = (TextView) findViewById(R.id.textViewSpeed);
                 tvLocation.setText(location.getLatitude() + ", " + location.getLongitude());
-                tvSpeed.setText(location.getSpeed() + " m/s");
+                tvSpeed.setText(getResources().getString(R.string.speed_unit_ms, location.getSpeed()));
             }
 
             @Override
@@ -127,6 +171,20 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
                     locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
                     return;
                 }
+        }
+    }
+
+    private void toggleEnergyDisplay() {
+        //ToDo: switch actual values in text views
+        if (energyDisplayFlag) {
+            energyDisplayFlag = false;
+            legendTimeCO2.setText(getResources().getString(R.string.legend_time));
+            legendDistanceEnergy.setText(getResources().getString(R.string.legend_distance));
+        }
+        else {
+            energyDisplayFlag = true;
+            legendTimeCO2.setText(getResources().getString(R.string.legend_co2));
+            legendDistanceEnergy.setText(getResources().getString(R.string.legend_energy));
         }
     }
 }
