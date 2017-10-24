d20135a1c8c05e1d6b6c8a5cb2f18d99ab281004
Jonathan Jan <jonathan.jan95@gmail.com>
2017-10-24 15:31:02 +0200

    Added methods to convert to co2 and energy usage and alertDialog

--- Android/app/src/main/java/ch/ethz/vs/se/team7/carbonfootprintmonitor/MainActivity.java
+++ Android/app/src/main/java/ch/ethz/vs/se/team7/carbonfootprintmonitor/MainActivity.java
@@ -3,6 +3,7 @@ package ch.ethz.vs.se.team7.carbonfootprintmonitor;
 import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -16,8 +17,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import android.app.Dialog;
+import android.app.AlertDialog;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -51,6 +59,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private TextView tramValue1;
     private TextView tramValue2;
 
+    //alertDialog for Transport Types
+    Button vehicle;
+    String[] listOfTransportTypes;
+    int transportTypeInt;
+    String transportTypeString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +80,55 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         tvSpeed = findViewById(R.id.textView_speed);
         tvLocation = findViewById(R.id.textView_location);
 
+        //AlertDialog. How often should we be able to ask this question?
+        vehicle = (Button) findViewById(R.id.vehicleDialogBtn);
+
+        //AlertDialog Items.
+        listOfTransportTypes = getResources().getStringArray(R.array.transport_types);
+
+
+
+//***************************************************************
+        vehicle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("What vehicle are you using?");
+                builder.setSingleChoiceItems(listOfTransportTypes, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int chosenTranportType) {
+                        //String is not needed as our convertion method uses INT.
+                        transportTypeString = listOfTransportTypes[chosenTranportType];
+                        transportTypeInt = chosenTranportType;
+
+                    }
+                });
+                builder.setCancelable(false);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        StringBuilder typeStringBuilder = new StringBuilder();
+                        typeStringBuilder.append(transportTypeString);
+                        typeStringBuilder.append(" selected");
+                        Toast.makeText(getApplicationContext(), typeStringBuilder, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+
+
+
+ //**************************************************************/
         // Initialize (CO2/energy) / (h/km) toggle button
         energyToggleButton = findViewById(R.id.button_toggle_energy);
         energyToggleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("MainActivity -> onClick", "energyToggleButton");
                 toggleEnergyDisplay();
             }
         });
@@ -187,4 +244,59 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             legendDistanceEnergy.setText(getResources().getString(R.string.legend_energy));
         }
     }
+
+
+    //Methods to convert kilometer and transport type to Primary Energy consumtion
+    //and CO2 emissions.
+    private double convertToEnergy(int transportType, double distance) {
+        /*0 = tram
+        * 1 = local bus
+        * 2 = regional train
+        * 3 = cable car
+        * 4 = boat
+        * 5 = S-Bahn
+        * Comment: We can add more veicles later on.
+        * */
+        double energyConsumption;
+        switch (transportType) {
+            case 0: energyConsumption = distance * 33.5; //Tram
+                break;
+            case 1: energyConsumption = distance * 47.2; //Local Bus
+                break;
+            case 2: energyConsumption = distance * 35.9; //Regional Train
+                break;
+            case 3: energyConsumption = distance * 118.0; //Cable car
+                break;
+            case 4: energyConsumption = distance * 51.4; //Boat
+                break;
+            case 5: energyConsumption = distance * 20.4; //S-bahn
+                break;
+            //Should have try catch here?
+            default: energyConsumption = 0;
+                break;
+        }
+        return energyConsumption;
+    }
+    private double convertToCO2(int transportType, double distance) {
+        double co2Consumption;
+        switch (transportType) {
+            case 0: co2Consumption = distance * 24.9; //Tram
+                break;
+            case 1: co2Consumption = distance * 100.3; //Local Bus
+                break;
+            case 2: co2Consumption = distance * 9.7; //Regional Train
+                break;
+            case 3: co2Consumption = distance * 63.9; //Cable car
+                break;
+            case 4: co2Consumption = distance * 119.2; //Cable car
+                break;
+            case 5: co2Consumption = distance * 8.2; //S-bahn
+                break;
+            //Should have try catch here?
+            default: co2Consumption= 0;
+                break;
+        }
+        return co2Consumption;
+    }
+
 }
